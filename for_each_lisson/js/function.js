a72fcc4b4eb84582989e134f303bf9d169e53096
HirokiMiyashita <79151937+HirokiMiyashita@users.noreply.github.com>
2021-02-16 23:30:15 +0900

    Add files via upload

---
+++ for_each_lisson/js/function.js
@@ -0,0 +1,61 @@
+// $(function(){
+// 	$('.box1').hover(
+// 	function(){
+// 		$('.box1').css({"background-Color": "#333333"});
+// 	}, function(){
+// 		$('.box1').css({"background-Color": "#8bc34a"});}
+
+// 	);
+// });
+
+// let box = document.querySelector('.box');
+
+//  document.querySelector('.top').addEventListener('mouseover',() => {
+// 	console.log('マウスオーバーしました');
+// 	box.style.display = "block";
+
+// });
+
+// document.querySelector('.top').addEventListener('mouseout',() => {
+	
+// 	box.style.display = "none";
+
+// });
+
+
+// let button_on = document.querySelector('.button_on');
+// document.querySelector('.button').addEventListener('click',() => {
+// 	console.log('ghjkl');
+// 	button_on.style.width = '500px';
+	
+
+// });
+
+// let hover = document.querySelector('.hover');
+
+// let hover = document.querySelector('.hover');
+// 	addEventListener('mouseover',() => {
+// 		console.log('ghjkl');
+// 		hover.style.transition = 1.0 ;
+// 		hover.style.backgroundColor = 'red';
+
+// });
+
+//  document.querySelector('.hover').addEventListener('mouseout',() => {
+// 		console.log('ghjkl');
+// 		hover.style.transition = 1.0 ;
+// 		hover.style.backgroundColor = 'black';
+
+// });
+
+
+
+
+
+
+
+
+      
+
+       
+      
